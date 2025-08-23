90e7d1a0ff971f06e7a0a8a19b8ffd92c63a25d3
habul17 <abulhassanca.dev@gmail.com>
2025-08-23 05:53:30 +0000

    Fix : Added Toggle Theme to the GitHub Link

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let isDark = true;
+const link = document.querySelector("a");
 
 function toggleColors() {
     if (isDark) {
         document.body.style.backgroundColor = "white";
         document.body.style.color = "black";
+        link.style.color = "black";
     } else {
         document.body.style.backgroundColor = "black";
         document.body.style.color = "white";
+        link.style.color = "white";
+
     }
     isDark = !isDark;
 }
